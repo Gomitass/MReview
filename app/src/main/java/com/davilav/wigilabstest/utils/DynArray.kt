e920d14ab04e8c4bc6fcc4047f90978cb310003b
Sebastian Gomez <68175554+Gomitass@users.noreply.github.com>
2022-01-13 21:51:37 -0500

    Add files via upload

---
+++ app/src/main/java/com/davilav/wigilabstest/utils/DynArray.kt
@@ -0,0 +1,31 @@
+class DynArray (var data: Array<Any?> = Array<Any?>(10){null}, var size: Int = 0){
+
+    fun isEmpty(): Boolean{
+        return size == 0
+    }
+
+    fun add(d:Any?){
+        if (size == data.size){
+            data = data.copyOf((size * 1.618).toInt())
+        }
+        data[size] = d
+        size++
+    }
+
+    fun remove(){
+        if (!isEmpty()){
+            size--
+            if (size > 10 && 2 * size <= data.size){
+                data = data.copyOf((data.size * 0.618).toInt())
+            }
+        }
+    }
+
+    fun get(index:Int): Any?{
+        return data[index]
+    }
+
+    fun set(d: Any?, index: Int){
+        data[index] = d
+    }
+}
---
+++ app/src/main/java/com/davilav/wigilabstest/utils/PrQueue.kt
@@ -0,0 +1,106 @@
+import kotlin.random.Random
+
+interface Priority{
+    fun get(d: Any?): Double
+}
+
+class PrQueue(var priority: Priority, var heap: DynArray = DynArray()){
+    fun enQueue(d: Any?){
+        var index = heap.size
+        var parent = (index - 1)/2
+        heap.add(d)
+        while(index != 0 && priority.get(heap.get(index)) > priority.get(heap.get(parent)) ){
+            var t = heap.get(index)
+            heap.set(heap.get(parent), index)
+            heap.set(t, parent)
+            index = parent
+            parent = (index - 1)/2
+        }
+    }
+
+    fun deQueue(): Any? {
+        if (!heap.isEmpty()) {
+            var d = heap.get(0)
+            heap.remove()
+            var index = heap.size
+            var t = heap.get(index)
+            heap.set(t, 0)
+            var parent = 0
+            var child = bestChild(parent)
+            var papr = priority.get(heap.get(parent))
+            while (child >= 0) {
+                var chpr = priority.get(heap.get(child))
+                if(papr >= chpr) {
+                    child = -1
+                }else{
+                    t = heap.get(parent)
+                    heap.set(heap.get(child), parent)
+                    heap.set(t, child)
+                    parent = child
+                    child = bestChild(parent)
+                }
+
+            }
+            return d
+        }
+        return null
+    }
+
+    fun bestChild(parent: Int): Int{
+        var left = (2 * parent) + 1
+        var right = (2 * parent) + 2
+        if(left >= heap.size){
+            return -1
+        }
+
+        if(right >= heap.size){
+            return left
+        }
+
+        var lepr = priority.get(heap.get(left))
+        var ripr = priority.get(heap.get(right))
+
+        if(lepr > ripr){
+            return left
+        }else{
+            return right
+        }
+    }
+
+    fun isEmpty(): Boolean{
+        return heap.isEmpty()
+    }
+    fun print(){
+        for(i in 0..(heap.size - 1)){
+            print(heap.get(i))
+            print(" / ")
+        }
+        print("\n")
+
+    }
+
+}
+
+class InPriority(): Priority{
+    override fun get(d: Any?): Double{
+        return (d as Int).toDouble()
+    }
+
+}
+fun main(args: Array<String>){
+    var queue = PrQueue(InPriority())
+    for (i in 0..29){
+        queue.enQueue(Random.nextInt(0, 100))
+        queue.print()
+    }
+    print("\n")
+    print(" Se borran datos de la cola")
+    print("\n")
+
+    while(!queue.isEmpty()){
+        var x = queue.deQueue()
+        print(x)
+        print("\n")
+        queue.print()
+    }
+}
